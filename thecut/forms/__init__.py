6c376a382862ebe05e843c1c4c934c98089b0ea2
Kye Russell <kye.russell@thecut.net.au>
2018-08-02 15:42:09 +0800

    Version bump

--- thecut/forms/__init__.py
+++ thecut/forms/__init__.py
@@ -8,13 +8,13 @@ except ImportError:
 
 __title__ = 'thecut-forms'
 
-__version__ = '0.5.1'
+__version__ = '0.6'
 
 __url__ = 'https://github.com/thecut/thecut-forms'
 
 __author__ = 'The Cut Creative <development@thecut.net.au>'
 
-__copyright__ = 'Copyright 2013-2017 Busara Perth Pty Ltd'
+__copyright__ = 'Copyright 2013-2018 Busara Perth Pty Ltd'
 
 __license__ = 'Apache 2.0'
 
